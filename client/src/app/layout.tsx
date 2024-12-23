efd483dfdadcbe426a71fca432d2d660b577bbc4
Herbert <herbetntim@gmail.com>
2024-12-23 16:24:53 +0000

    added state providers

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -20,10 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.variable} antialiased`}
-      >
-        {children}
+      <body className={`${dmSans.className} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
