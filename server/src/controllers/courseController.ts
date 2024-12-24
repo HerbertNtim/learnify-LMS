4b8a7cfc5eab9eca53e178a2cab8c4fabbc2c4e5
Herbert <herbetntim@gmail.com>
2024-12-24 23:26:32 +0000

    added a controler to get a course

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import Course from '../models/courseModel'
 
-export const listCourse = async (req: Request, res: Response): Promise<void> => {
+export const listCourses = async (req: Request, res: Response): Promise<void> => {
   const { category } = req.query
   try {
     const courses =  category && category !== "all" ? await Course.scan("category").eq(category).exec() : await Course.scan().exec();
@@ -9,5 +9,19 @@ export const listCourse = async (req: Request, res: Response): Promise<void> => 
     res.status(200).json({ message: "Courses retrieved successfully ", courses })
   } catch (error) {
     res.status(500).json({ message: "Error retrieving courses", error });
+  }
+}
+
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+  const { courseId } = req.params;
+  try {
+    const course =  await Course.get(courseId);
+    if(!course) {
+      res.status(404).json({ message: "Course not found"})
+    }
+
+    res.status(200).json({ message: "Courses retrieved successfully ", course })
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving course", error });
   }
 }
