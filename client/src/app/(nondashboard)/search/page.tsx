bcb35dc36951a8a542fb7d77002fdde991e8bf18
Herbert <herbertntim15@gmail.com>
2024-12-28 10:47:58 +0000

    added getting search courses

---
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useGetCoursesQuery } from "@/state/api"
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState } from "react"
+
+const Search = () => {
+  const searchParams = useSearchParams()
+  const id = searchParams.get("id")
+
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({})
+  const [ selectedCourse, setSelectedCourse ] = useState<Course | null>(null)
+
+  useEffect(() => {
+    if(courses) {
+      if(id) {
+        const course = courses.find((c) => c.courseId === id)
+        setSelectedCourse(course || courses[0])
+      } else {
+        setSelectedCourse(courses[0])
+      }
+    }
+  }, [courses, id])
+
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
