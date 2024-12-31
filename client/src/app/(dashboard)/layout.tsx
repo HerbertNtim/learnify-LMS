1d3efee0ef084f082f441070907eb5b6f5c89b7b
Herbert <herbertntim15@gmail.com>
2024-12-31 18:03:06 +0000

    added dashboard navbar

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -6,27 +6,26 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const [courseId, setCourseId] = useState<string | null>(null);
+  // const [courseId, setCourseId] = useState<string | null>(null);
   const { user, isLoaded } = useUser();
   const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
     pathname
   );
 
-  useEffect(() => {
-    if (isCoursePage) {
-      const match = pathname.match(/\/user\/courses\/([^\/]+)/);
-      setCourseId(match ? match[1] : null);
-    } else {
-      setCourseId(null);
-    }
-  }, [isCoursePage, pathname]);
+  // useEffect(() => {
+  //   if (isCoursePage) {
+  //     const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+  //     setCourseId(match ? match[1] : null);
+  //   } else {
+  //     setCourseId(null);
+  //   }
+  // }, [isCoursePage, pathname]);
 
   if (!isLoaded) return <Loading />;
   if (!user) return <div>Please sign in to access this page.</div>;
