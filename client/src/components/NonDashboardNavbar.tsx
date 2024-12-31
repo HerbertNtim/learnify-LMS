22d7898dd258fa43cdb655df8cc120e9a23381f8
Herbert <herbertntim15@gmail.com>
2024-12-31 15:54:30 +0000

    updated tailwind config and added user button

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -1,34 +1,82 @@
-import { Bell, BookOpen } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import { SignedIn, SignedOut, UserButton, useUser } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Bell, BookOpen } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const NonDashboardNavbar = () => {
+  const { user } = useUser();
+  const userRole = user?.publicMetadata?.userType as "student" | "teacher";
+
+
   return (
-    <nav className='nondashboard-navbar'>
-      <div className='nondashboard-navbar__container'>
-        <div className='nondashboard-navbar__search'>
-          <Link href={'/'} className='nondashboard-navbar__brand'>LEARNIFY</Link>
-          <div className='flex items-center gap-4'>
-            <div className='relative group'>
-              <Link href={'/search'} className='nondashboard-navbar__search-input'>
-                <span className='hidden sm:inline'>Search Courses</span>
-                <span className='sm:hidden'>Search</span>
+    <nav className="nondashboard-navbar">
+      <div className="nondashboard-navbar__container">
+        <div className="nondashboard-navbar__search">
+          <Link href={"/"} className="nondashboard-navbar__brand">
+            LEARNIFY
+          </Link>
+          <div className="flex items-center gap-4">
+            <div className="relative group">
+              <Link
+                href={"/search"}
+                className="nondashboard-navbar__search-input"
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
               </Link>
-              <BookOpen size={18} className='nondashboard-navbar__search-icon'/>
+              <BookOpen
+                size={18}
+                className="nondashboard-navbar__search-icon"
+              />
             </div>
           </div>
         </div>
-        <div className='nondashboard-navbar__actions'>
-          <button className='nondashboard-navbar__notification-button'>
-            <span className='nondashboard-navbar__notification-indicator'></span>
-            <Bell className='nondashboard-navbar__notification-icon' />
+        <div className="nondashboard-navbar__actions">
+          <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span>
+            <Bell className="nondashboard-navbar__notification-icon" />
           </button>
+
+          {/* AUTH BUTTONS */}
+          <SignedIn>
+            <UserButton appearance={{
+                baseTheme: dark,
+                elements: {
+                  userButtonOuterIdentifier: "text-customgreys-dirtyGrey",
+                  userButtonBox: "scale-90 sm:scale-100",
+                },
+              }}
+              showName={true}
+              userProfileMode="navigation"
+              userProfileUrl={
+                userRole === "teacher" ? "/teacher/profile" : "/user/profile"
+              } />
+          </SignedIn>
+
+          <SignedOut>
+            <Link
+              href={"/signin"}
+              className="nondashboard-navbar__auth-button--login"
+              scroll={false}
+            >
+              Log In
+            </Link>
+
+            <Link
+              href={"/signin"}
+              className="nondashboard-navbar__auth-button--signup"
+              scroll={false}
+            >
+              Sign Up
+            </Link>
+          </SignedOut>
         </div>
       </div>
-
-      {/* SIGN IN BUTTONS */}
     </nav>
-  )
-}
+  );
+};
 
-export default NonDashboardNavbar
+export default NonDashboardNavbar;
