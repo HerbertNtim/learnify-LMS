98fc84ec69a0105a60bf0087aae1258d65aceebc
Herbert <herbertntim15@gmail.com>
2025-01-01 12:18:50 +0000

    added user import from clerk

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
+import { User } from "@clerk/nextjs/server"
 
 const customBaseQuery = async (
   args: string | FetchArgs,
