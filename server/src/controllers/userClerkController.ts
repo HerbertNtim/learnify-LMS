d0eb94f971bd0d15843d8a589f2836b373bea1bf
Herbert <herbertntim15@gmail.com>
2025-01-01 13:03:55 +0000

    updated clerk controller

--- server/src/controllers/userClerkController.ts
+++ server/src/controllers/userClerkController.ts
@@ -1,5 +1,3 @@
-
-
 import { Request, Response } from 'express';
 import { clerkClient } from '../index';
 
@@ -8,14 +6,14 @@ export const updateUser = async (req: Request, res: Response): Promise<void> => 
   const userData = req.body;
 
   try {
-    await clerkClient.users.updateUserMetadata( userId, {
+    const user = await clerkClient.users.updateUserMetadata( userId, {
       publicMetadata: {
         userType: userData.publicMetadata.userType,
         settings: userData.publicMetadata.settings
       }
     })
 
-    res.status(200).json({ message: "User updated successfully" })
+    res.status(200).json({ message: "User updated successfully", data: user })
   } catch (error) {
     res.status(500).json({ message: "Error updating user", error })
   }
