86be99742c28cfbd628eddab44395d4eeda27d3c
Herbert <herbertntim15@gmail.com>
2025-01-03 01:41:25 +0000

    updated api custom base query

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -4,6 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
 import { User } from "@clerk/nextjs/server"
 import { Clerk }  from "@clerk/clerk-js";
+import { toast } from "sonner";
 
 const customBaseQuery = async (
   args: string | FetchArgs,
@@ -25,9 +26,28 @@ const customBaseQuery = async (
   try {
     const result: any = await baseQuery(args, api, extraOptions);
 
-    if (result.dat) {
-      return (result.data = result.data.data);
+    if(result.error) {
+      const errorData = result.error.data;
+      const errorMessage = errorData?.message || result.error.status.toString() || "An error occurred";
+      toast.error(`Error: ${errorMessage}`); 
     }
+
+    const isMutationRequest =
+      (args as FetchArgs).method && (args as FetchArgs).method !== "GET";
+
+    if (isMutationRequest) {
+      const successMessage = result.data?.message;
+      if (successMessage) toast.success(successMessage);
+    }
+
+    if (result.data) {
+      return (result.data = result.data.data);
+    } else if (result.error?.status === 204 || result.meta?.response.status === 204) {
+      return {
+        data: null,
+      };
+    }
+  
 
     return result;
   } catch (error) {
