7299db34e19b60c3f623bdacee2de667c9914cf6
Herbert <herbertntim15@gmail.com>
2025-01-04 17:29:59 +0000

    added checkout hook

--- client/src/app/(nondashboard)/checkout/page.tsx
+++ client/src/app/(nondashboard)/checkout/page.tsx
@@ -2,11 +2,13 @@
 
 import Loading from '@/components/Loading';
 import WizardStepper from '@/components/WizardStepper';
+import { useCheckoutNavigation } from '@/hooks/useCheckoutNavigation';
 import { useUser } from '@clerk/nextjs'
 import React from 'react'
 
 const CheckoutWizard = () => {
   const { isLoaded } = useUser();
+  const { checkoutStep } = useCheckoutNavigation();
 
   if (!isLoaded) {
     return <Loading />;
@@ -26,8 +28,7 @@ const CheckoutWizard = () => {
   }
 
   return (
-    <WizardStepper currentStep={checkoutStep} />
-    <div className='checkout'>{renderStep()}</div>
+    <div className='checkout'><WizardStepper currentStep={checkoutStep} />{renderStep()}</div>
   )
 }
 
