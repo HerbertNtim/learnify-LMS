e3af3ae92fcda9ece1ff7616f83fcebf136df676
Herbert <herbertntim15@gmail.com>
2025-01-09 12:51:46 +0000

    added stripe provider and payment page

---
+++ client/src/app/(nondashboard)/checkout/payment/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const PaymentPageContent = () => {
+  return (
+    <div>PaymentPageContent</div>
+  )
+}
+
+const PaymentPage = () => {
+  return (
+    <div>
+      <PaymentPageContent />
+    </div>
+  )
+}
+
+export default PaymentPage
