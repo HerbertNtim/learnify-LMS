4338834b3a77a1a22e5bb5608f2396c898ed200b
Herbert <herbertntim15@gmail.com>
2025-01-14 08:28:03 +0000

    implemented create course in course controller

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import Course from '../models/courseModel'
+import { v4 as uuidv4 } from 'uuid';
 
 export const listCourses = async (req: Request, res: Response): Promise<void> => {
   const { category } = req.query
@@ -21,6 +22,37 @@ export const getCourse = async (req: Request, res: Response): Promise<void> => {
     }
 
     res.status(200).json({ message: "Courses retrieved successfully ", data: course })
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving course", error });
+  }
+}
+
+export const createCourse = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { teacherId, teacherName } = req.body;
+
+    if (!teacherId || !teacherName) {
+      res.status(400).json({ message: "Please provide teacherId and teacherName" });
+      return;
+    }
+
+    const newCourse = new Course({
+      courseId: uuidv4(),
+      teacherId,
+      teacherName,
+      title: "Untitled Course",
+      description: "",
+      category: "No categorized",
+      image: '',
+      price: 0,
+      level: "Beginner",
+      status: "Draft",
+      sections: [],
+      enrollment: []
+    })
+    await newCourse.save();
+
+    res.status(200).json({ message: "Course created successfully ", data: newCourse })
   } catch (error) {
     res.status(500).json({ message: "Error retrieving course", error });
   }
