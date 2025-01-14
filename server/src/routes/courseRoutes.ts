430a6432ae3510b48c199eb5bc5bc210ecf7e105
Herbert <herbertntim15@gmail.com>
2025-01-14 10:30:10 +0000

    added routes to courses router

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getCourse, listCourses } from "../controllers/courseController";
+import { createCourse, deleteCourse, getCourse, listCourses, updateCourse } from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
+import multer from "multer";
 
 const courseRouter = Router()
 
+const upload = multer({ storage: multer.memoryStorage() });
+
 courseRouter.get("/", listCourses)
 courseRouter.get("/:courseId", getCourse)
+courseRouter.post("/", requireAuth(), createCourse)
+courseRouter.put("/:courseId", requireAuth(), upload.single("image"), updateCourse)
+courseRouter.delete("/:courseId", requireAuth(), deleteCourse)
 
 export default courseRouter
