c572451d4c594efe2aeebd49b95f6a883f65c4ca
Herbert <herbertntim15@gmail.com>
2025-01-20 11:31:59 +0000

    added user progress upadate controller

--- server/src/controllers/userCourseProgressController.ts
+++ server/src/controllers/userCourseProgressController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import UserCourseProgress from "../models/userCourseProgressModel";
 import Course from "../models/courseModel";
+import { calculateOverallProgress, mergeSections } from "../utils/utils";
 
 export const getUserEnrolledCourses = async (req: Request, res: Response) => {
   const { userId } = req.params;
@@ -36,6 +37,45 @@ export const getUserCourseProgress = async (req: Request, res: Response) => {
     res.status(500).json({
       message: "Failed to fetch user's course progress",
       error: error.message,
+    });
+  }
+}
+
+export const updateUserCourseProgress = async (req: Request, res: Response) => {
+  const { userId, courseId } = req.params;
+  const progressData = req.body;
+
+  try {
+    let progress = await UserCourseProgress.get({ userId, courseId });
+
+    if (!progress) {
+      progress = new UserCourseProgress({ 
+        userId, 
+        courseId,
+        enrollmentDate: new Date().toISOString(),
+        overallProgress: 0,
+        sections: progressData.section || [],
+        lastAccessedTimestamp: new Date().toISOString(), 
+      });
+    } else {
+      progress.sections = mergeSections(
+        progress.sections,
+        progressData.section || []
+      );
+      progress.lastAccessedTimestamp = new Date().toISOString();
+      progress.overallProgress = calculateOverallProgress(progress.sections);
+    }
+
+    await progress.save();
+
+    res.status(200).json({
+      message: "Successfully updated user's course progress",
+      data: progress,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update user's course progress",
+      error
     });
   }
 }
