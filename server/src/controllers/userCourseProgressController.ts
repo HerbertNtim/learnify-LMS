aafbe552d6745b4b0e315bd5952f051e84062f60
Herbert <herbertntim15@gmail.com>
2025-01-20 10:28:32 +0000

    created course progress controller and added getUserEnrolledCourses function to get all user courses

---
+++ server/src/controllers/userCourseProgressController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import UserCourseProgress from "../models/userCourseProgressModel";
+import Course from "../models/courseModel";
+
+export const getUserEnrolledCourses = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    const enrolledCourses = await UserCourseProgress.query("userId").eq(userId).exec();
+    const courseIds = enrolledCourses.map((course: any) => course.courseId);
+    const courses = await Course.batchGet(courseIds);
+    
+    res.status(200).json({
+      message: "Successfully fetched user's enrolled courses",
+      data: courses,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: "Failed to fetch user's enrolled courses",
+      error: error.message,
+    });
+  }
+}
