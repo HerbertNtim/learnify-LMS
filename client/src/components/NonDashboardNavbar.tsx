d50d5ea03baa28308f509f0e86c3b0e0c62279ec
Herbert <herbertntim15@gmail.com>
2025-01-23 13:10:42 +0000

    user

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 const NonDashboardNavbar = () => {
   const { user } = useUser();
-  const userRole = user?.publicMetadata?.userType as "student" | "teacher";
+  const userRole = user?.publicMetadata?.userType as "user" | "teacher";
 
   return (
     <nav className="nondashboard-navbar">
