80ccbd1fcf6e2fb3af28d3f66c2695fcacc4b6b2
Herbert <herbertntim15@gmail.com>
2025-01-23 11:03:07 +0000

    working fine

--- client/src/app/(dashboard)/teacher/courses/page.tsx
+++ client/src/app/(dashboard)/teacher/courses/page.tsx
@@ -68,7 +68,7 @@ const Courses = () => {
   };
 
   if (isLoading) return <Loading />;
-  if (isError || !courses) return <Error isError={isError} courses={courses} title="No Course created"/>;
+  if (isError || !courses) return <Error isError={isError} courses={courses} />;
 
   return (
     <div className="teacher-courses">
